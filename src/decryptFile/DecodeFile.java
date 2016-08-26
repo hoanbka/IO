149fa6ccea6d061ec68b6f8955471252219ceec0
Albert <nguyenhoan.hut@gmail.com>
2016-08-27 02:01:04 +0700

    FileDecryption

--- src/decryptFile/DecodeFile.java
+++ src/decryptFile/DecodeFile.java
@@ -16,19 +16,16 @@ public class DecodeFile {
         byte[] bytes = new byte[1024];
 
         while (bufferedInputStream.read(bytes) > 0) {
-            for (byte b : bytes
-                    ) {
-                if (b != 0) {
-                    System.out.print((char) (byte) (b - 5));
-                    list.add((byte) (b - 5));
-                }
+            for (byte b : bytes) {
+                System.out.print((char) (byte) (b - 5));
+                list.add((byte) (b - 5));
             }
         }
         bufferedInputStream.close();
     }
 
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\NguyenDinhLong\\IdeaProjects\\EncodingTextFile\\TextFile.txt_encrypted2.txt");
+        File file = new File("C:\\Users\\NguyenDinhLong\\IdeaProjects\\IOProject\\TextFile.txt_encrypted2.txt");
         try {
             decodeFile(file);
         } catch (IOException e) {
