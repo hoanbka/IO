4870555a692cf5f2398b5764fe8380d29101b698
Albert <nguyenhoan.hut@gmail.com>
2016-08-29 22:01:05 +0700

    IO

--- src/student/management/StudentDataAccess.java
+++ src/student/management/StudentDataAccess.java
@@ -4,48 +4,71 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class StudentDataAccess {
 
     public static void writeToBinaryFile(List<Student> studentList) throws IOException {
+
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("Binary.txt", true));
-        objectOutputStream.writeObject(studentList);
-        objectOutputStream.flush();
-        objectOutputStream.close();
+        try {
+            objectOutputStream.writeObject(studentList);
+            objectOutputStream.flush();
+
+        } finally {
+            objectOutputStream.close();
+        }
+
+    }
+
+    public static void writeToBinaryFile2(List<Student> studentList) throws IOException {
+
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("Binary.txt", true))) {
+            objectOutputStream.writeObject(studentList);
+            objectOutputStream.flush();
+        }
+
     }
 
     public static void readFromBinaryFile(String path) throws IOException, ClassNotFoundException {
-        ObjectInputStream objctInputStream = new ObjectInputStream(new FileInputStream(path));
-        List<Student> studentList = (List<Student>) objctInputStream.readObject();
+        try (ObjectInputStream objctInputStream = new ObjectInputStream(new FileInputStream(path))) {
 
-        for (Student std : studentList) {
-            System.out.println(std.toString());
+            List<Student> studentList = (List<Student>) objctInputStream.readObject();
+
+            for (Student std : studentList) {
+                System.out.println(std.toString());
+            }
         }
-        objctInputStream.close();
     }
 
     public static void writeToTextFile(List<Student> studentList) throws FileNotFoundException, UnsupportedEncodingException {
+
         PrintWriter writer = new PrintWriter("StudentList.txt", "UTF-8");
-        for (Student stdList : studentList) {
-            writer.println(stdList.toString());
+        try {
+
+            for (Student stdList : studentList) {
+                writer.println(stdList.toString());
+            }
+
+        } finally {
+            writer.close();
         }
-        writer.close();
     }
 
     public static List<Student> readFromTextFile(String path) throws IOException {
         List<Student> students = new ArrayList<>();
         File file = new File(path);
 
-        BufferedReader inputStream = new BufferedReader(new FileReader(file));
-        String line;
-        while ((line = inputStream.readLine()) != null) {
-            String splitted[] = line.split(";");
-            String name = splitted[0];
-            int age = Integer.parseInt(splitted[1]);
-            String ID = splitted[2];
-            students.add(new Student(name, age, ID));
+        try (BufferedReader inputStream = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = inputStream.readLine()) != null) {
+                String splitted[] = line.split(";");
+                String name = splitted[0];
+                int age = Integer.parseInt(splitted[1]);
+                String ID = splitted[2];
+                students.add(new Student(name, age, ID));
+            }
+            return students;
         }
-        inputStream.close();
-        return students;
     }
 
 
